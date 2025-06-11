2517a422c0a2a11e6442d88ad8b1fd419c0c0e13
rxshellg <157666982+rxshellg@users.noreply.github.com>
2025-06-11 04:34:20 -0400

    Added footer (#6)
    Co-authored-by: rxshellg <rashell-gb@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Calculator from './pages/Calculator';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Calculator" element={<Calculator />} />
     </Routes>
+      <Footer />
+    </>
   )
 };
 
