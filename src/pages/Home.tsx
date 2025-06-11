5d58cc27567f82f20ca2304ce1e1a6819173a442
rxshellg <157666982+rxshellg@users.noreply.github.com>
2025-06-11 00:20:08 -0400

    Completed Home page layout- TBF (#4)
    Co-authored-by: rxshellg <rashell-gb@hotmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import useIsMobile from "../hooks/useIsMobile";
+
+import './Home.css';
+
 const Home = () => {
+    const isMobile = useIsMobile();
+    const button = (<Link to="/Calculator"><button className="getStartedButton">Get started</button></Link>)
+
     return (
-        <p>Home</p>
+        <>
+            <div className={isMobile ? "mobileHome" : "desktopHome"}>
+                <div className="textContainer">
+                    <h1>Bill buddy</h1>
+                    <h2>For when the check comes and no one wants to think.
+                    Enter your numbers, split the bill, done.</h2>
+                    {!isMobile && button}
+                </div>
+                <div className="imageContainer">
+                </div>
+                {isMobile && button}
+            </div>
+        </>
     )
 }
 
